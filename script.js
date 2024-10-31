d6b470e90a90ead44d927410d343ee1e1d20380c
Stakshi <128694806+Stakshi301@users.noreply.github.com>
2024-10-31 19:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,20 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  let price=document.querySelectorAll('.price');
+	price=Array.from(price);
+	let sum=price.reduce((total,price)=>
+	total+Number(price.innerText),0);
+
+	let table=document.querySelector('table');
+	let row=document.createElement('tr');
+	let cell=document.createElement('td');
+	cell.colspan=2;
+	cell.innerText=`Total: ${sum}`;
+	row.appendChild(cell);
+	table.appendChild(row);
 };
+
 
 getSumBtn.addEventListener("click", getSum);
 
